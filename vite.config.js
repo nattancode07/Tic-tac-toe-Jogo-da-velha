7571bd4429950153f9090e37ff908a146e719c47
Elyaquim Nattan <elyaquimnattan@gmail.com>
2025-05-12 15:16:27 -0300

    ajustes 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<Tic-tac-toe-Jogo-da-velha>/',
+  base: '/Tic-tac-toe-Jogo-da-velha/',
   plugins: [react()],
 })
